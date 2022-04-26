fc465ee40213383da2d1dbee716a17103b0441bf
masuda <92622872+ponpongula@users.noreply.github.com>
2022-04-26 10:06:25 +0900

    Update sql4-5.php
    test

--- src/public/sql4-5.php
+++ src/public/sql4-5.php
@@ -20,15 +20,6 @@ echo "収出の低い順にsortして月ごとの収入の合計を一覧表示"
 foreach($data_sort->revenue_low_first() as $key => $value){
   echo $key . "月：" . $value . "<br>"; 
 }
-// var_dump($data_sort->revenue_highest_first());
-// print_r($data_sort->revenue_highest_first());
-// foreach($data_sort->revenue_highest_first() as $key => $row){
-//   $date[$key] = $row["amount"];
-// }
-// array_multisort($date, SORT_DESC, $data_sort->revenue_highest_first());
-// foreach($data_sort->revenue_highest_first() as $sort) {
-//   echo $sort['amount'] . "<br>";
-// }
 
 class data_sort
 {
@@ -42,18 +33,6 @@ class data_sort
   public function revenue_highest_first(): array
   { 
     $result = [
-      "01" =>0,
-      "02" =>0,
-      "03" =>0,
-      "04" =>0,
-      "05" =>0,
-      "06" =>0,
-      "07" =>0,
-      "08" =>0,
-      "09" =>0,
-      "10" =>0,
-      "11" =>0,
-      "12" =>0
     ];
     foreach($this->incomes as $value) {
       [$year, $months, $date] = explode("-", $value['accrual_date']);
@@ -66,18 +45,6 @@ class data_sort
   public function revenue_low_first(): array
   { 
     $result = [
-      "01" =>0,
-      "02" =>0,
-      "03" =>0,
-      "04" =>0,
-      "05" =>0,
-      "06" =>0,
-      "07" =>0,
-      "08" =>0,
-      "09" =>0,
-      "10" =>0,
-      "11" =>0,
-      "12" =>0
     ];
     foreach($this->incomes as $value) {
       [$year, $months, $date] = explode("-", $value['accrual_date']);

8400a4c9d13640bd1a09ac2b980e91da7d328c07
hayato <rqspss1359@gmail.com>
2022-05-06 21:13:13 +0900

    編集

--- src/public/sql5-5~6.php
+++ src/public/sql5-5~6.php
@@ -8,15 +8,15 @@ $statement = $pdo->prepare($sql);
 $statement->execute();
 $spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$conditional_processing = new ConditionalProcessing($spendings);
+$ConditionalProcessing = new ConditionalProcessing($spendings);
 echo "支出の低い順sortして月ごとの支出の合計を一覧表示。ただし、支出日に5が含まれているときだけ100円引いてください。" . "<br>";
-foreach ($conditional_processing->revenueHighestFirst() as $key=>$value)
+foreach ($ConditionalProcessing->revenueHighestFirst() as $key=>$value)
 {
   echo $key . "月の支出の合計：" . $value . "<br>"; 
 }
 echo "<br>";
 echo "支出の高い順sortして月ごとの支出の合計を一覧表示。ただし、支出日に5が含まれているときだけ3000円引いてください。" . "<br>";
-foreach ($conditional_processing->revenueLowFirst() as $key=>$value)
+foreach ($ConditionalProcessing->revenueLowFirst() as $key=>$value)
 {
   echo $key . "月の支出の合計：" . $value . "<br>"; 
 }
@@ -50,7 +50,7 @@ class ConditionalProcessing
     foreach ($this->spendings as $value) {
       [$year, $months, $date] = explode("-", $value['accrual_date']);
       $result[$months] += $value['amount'];
-      if(preg_match("/5/", $date)) {
+      if (preg_match("/5/", $date)) {
         $result[$months] -= 3000;
         }
     }

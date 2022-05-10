ee40300237474357fdcb0e9c386358265da1e4b8
hayato <rqspss1359@gmail.com>
2022-05-10 19:34:32 +0900

    編集３

--- src/public/sql5-5~6.php
+++ src/public/sql5-5~6.php
@@ -10,12 +10,12 @@ $spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $conditionalProcessing = new ConditionalProcessing($spendings);
 echo "支出の低い順sortして月ごとの支出の合計を一覧表示。ただし、支出日に5が含まれているときだけ100円引いてください。" . "<br>";
-foreach ($ConditionalProcessing->revenueHighestFirst() as $key=>$value) {
+foreach ($conditionalProcessing->revenueHighestFirst() as $key=>$value) {
   echo $key . "月の支出の合計：" . $value . "<br>"; 
 }
 echo "<br>";
 echo "支出の高い順sortして月ごとの支出の合計を一覧表示。ただし、支出日に5が含まれているときだけ3000円引いてください。" . "<br>";
-foreach ($ConditionalProcessing->revenueLowFirst() as $key=>$value) {
+foreach ($conditionalProcessing->revenueLowFirst() as $key=>$value) {
   echo $key . "月の支出の合計：" . $value . "<br>"; 
 }
 
